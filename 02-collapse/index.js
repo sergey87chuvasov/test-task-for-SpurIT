554637ed3e73536c58ab52e5779fd6ce1ebe3e6e
Sergey87 <serge.techno@gmail.com>
2022-08-30 22:37:52 +0300

    added task 3

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,16 @@
 /* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+
+var collapsibleRotating = [{ opacity: 0 }, { opacity: 1 }];
+
+let btn = document.querySelector('.collapsible__button');
+
+btn.addEventListener('click', funkHide);
+
+let collTiming = {
+  duration: 1000,
+  iterations: Infinity,
+};
+
+var collapsibleAnimation = document
+  .querySelector('.collapsible__content')
+  .animate(collapsibleRotating, collTiming);
