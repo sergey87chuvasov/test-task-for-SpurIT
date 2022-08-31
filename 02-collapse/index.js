91f63c34ae9e21c9e6f47776f01221af3c618342
Sergey87 <serge.techno@gmail.com>
2022-08-31 22:43:51 +0300

    fixed tesk 2

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,20 +1,18 @@
 const collBtn = document.querySelector('.collapsible__button');
 
-collBtn.addEventListener('click', hideFunc);
+const animCollapse = document
+  .querySelector('.collapsible__content')
+  .animate(
+    { transform: 'translateY(-800px)' },
+    { duration: 250, fill: 'both', easing: 'ease-out' }
+  );
 
-function hideFunc() {
-  const aliceTumbling = [
-    { transform: 'translateY(0)' },
-    { transform: 'translateY(-480%)' },
-  ];
+animCollapse.pause();
 
-  let collTiming = {
-    duration: 1000,
-  };
-
-  document
-    .querySelector('.collapsible__content')
-    .animate(aliceTumbling, collTiming);
-}
-
-hideFunc();
+collBtn.addEventListener('click', () => {
+  if (animCollapse.playState === 'paused') {
+    animCollapse.play();
+  } else {
+    animCollapse.reverse();
+  }
+});
